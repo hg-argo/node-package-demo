ac4044c5db0a3ec4b6e4d15d12cf4fb68138a69d
Hubert Grossin <hubert@ar-go.co>
2026-03-22 19:21:54 +0100

    chore: Move "random" features in dedicated script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- */
-export function randomInt(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
+export * from './random'
